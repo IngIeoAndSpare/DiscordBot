d5264b51169449ababd1489f23cac6fb6006720f
WI-Hyun-joong <wjh4877@naver.com>
2019-04-09 21:30:42 +0900

    const 수정. 변수 명 수정

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 // ===== module ======================
-// const discord = require('discord.js'),
-//       axios = require('axios'),
-//       Bufferutil = require('bufferutil'),
-//       crypto = require('crypto');
+const discord = require('discord.js'),
+      axios = require('axios'),
+      bufferutil = require('bufferutil'),
+      crypto = require('crypto');
 
 
-const Discord = require('discord.js');
 var token = require('./properties/token.json');
-      
-var client = new Discord.Client();
+var client = new discord.Client();
 
 // 토큰으로 로그인
 client.login(token.botToken);
 
 
-//
 client.on("ready", () => {
     console.log('Server ON ');
 });
@@ -22,7 +19,8 @@ client.on("ready", () => {
 client.on("message", message => {
     console.log(message.content);
     // 입력받은 채팅 확인
-
+    // TODO : 각 content 프로퍼티 파일로 지정하여 불러오는 형식으로 실행.
+    // XXX : function => prop[message]
     if(message.content == "~"+"ping"){
         message.reply('pong');
         
